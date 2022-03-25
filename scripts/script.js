220d1bf2d769c403eea6cf1c774629b0ffc48286
Ismael Laet <ismaelabashi@hotmail.com>
2022-03-25 19:28:47 -0300

    Add responsive.css and Att script.js

--- scripts/script.js
+++ scripts/script.js
@@ -6,92 +6,91 @@ const Modal = {
     }
 }
 
+const Storage = {
+    set(obj) {
+        Object.keys(obj).forEach((item) => {
+            localStorage.setItem(`${item}`, JSON.stringify(obj[item])) 
+        })
+    },
+
+      
+
+    get(list) {
+        return JSON.parse(localStorage.getItem(list)) || [];
+    }
+}
+
+
 const Category = {
     showCategory(category) {
-        List.clearList();
+        const list = List.get(category);
 
-        List.getList(category).forEach((task, index) => {
-            DOM.addTask(task, index)
+        Category.active(category);
+        List.clear();
+
+        list.forEach((task, index) => {
+            
+            DOM.addTask(task, index);
+            DOM.checkTask(task, index);
+            DOM.eventCheck(task, index);   
+            Storage.set(List.all)   
         })
+    },
+
+    active(category) {
+        Category.removeActive();
+
+        document.querySelector(`#${category}`).classList.add('active');
+    },
+
+    removeActive() {
+        const categories = document.querySelector('.categories-list').children
+       for(index = 0; index < categories.length; index++) {
+           categories[index].classList.remove('active')
+       } 
     }
 }
 
 const List ={
-    workList: [
-        {
-            checked: true,
-            description: "Reunião",
-            time: "19:00"
-        },
-        {
-            checked: true,
-            description: "Bater ponto",
-            time: "08:00"
-        }
-    ],
+    all: {
+        workList: Storage.get('workList'),
 
-    healthList: [
-        {
-            checked: false,
-            description: "Marcar nutricionista",
-            time: ""
-        },
-        {
-            checked: false,
-            description: "Dentista",
-            time: "09:00"
-        }
-    ],
-    
-    personalList: [
-        {
-            checked: false,
-            description: "Academia",
-            time: "11:00"
-        }
-    ],
-    
-    shoppingList: [
-        {
-            checked: false,
-            description: "Feijão",
-            time: "",
-        },
-        {
-            checked: false,
-            description: "Arroz",
-            time: "",
-        },
-        {
-            checked: false,
-            description: "Ovo",
-            time: "",
-        },
-    ],
+        healthList: Storage.get('healthList'),
+        
+        personalList: Storage.get('personalList'),
+        
+        shoppingList: Storage.get('shoppingList')
+    },
 
-    getList(category) {
+    get(category) {
         let list;
         
         switch(category) {
             case 'category-work':
-                list = List.workList;
+                list = List.all.workList;
                 break;        
             case 'category-health':
-                list = List.healthList;
+                list = List.all.healthList;
                 break;        
             case 'category-personal':
-                list = List.personalList;
+                list = List.all.personalList;
                 break;        
             case 'category-shopping':
-                list = List.shoppingList;
+                list = List.all.shoppingList;
                 break;        
         }
 
         return list;
     },
 
-    clearList() {
+    clear() {
         DOM.unorderedList.innerHTML = "";
+    },
+
+    insert(task, category) {
+        List.get(category).push(task);
+        
+        Category.showCategory(category);
     }
 }
 
@@ -100,14 +99,14 @@ const DOM = {
 
     addTask(task, index) {
         const li = document.createElement('li');
-
         li.classList.add('task');
+        
         li.innerHTML = DOM.htmlTask(task, index);
         DOM.unorderedList.appendChild(li)
     },
 
     htmlTask(task, index) {
-        const html = `<label for="task${index}" class="task-text">
+        const html = `<label for="task${index}" class="task-text" >
             <input type="checkbox" id="task${index}">
             <span class="checkmark"></span>
     
@@ -118,7 +117,58 @@ const DOM = {
         return html;
     },
 
-    checkTask() {
-        
+    checkTask(task, index) {
+        const element = document.querySelector(`#task${index}`);
+
+        task.checked ? element.setAttribute('checked', 'true') : element.removeAttribute('checked')
+    },
+
+    eventCheck(task, index) {
+        const element = document.querySelector(`#task${index}`);
+
+        element.addEventListener('click', () => {
+
+            task.checked = !task.checked
+            DOM.checkTask(task, index);
+            Storage.set(List.all)
+        })       
     }
 }
+
+const Form = {
+    description: document.querySelector('#description-input'),
+    time: document.querySelector('#time-input'),
+    category: document.querySelector('#categories-select'),
+
+    getValues() {
+        return {
+            description: Form.description.value,
+            time: Form.time.value,
+            checked: false,
+            category: Form.category.value
+        }
+    },
+
+    clearFields() {
+        Form.description.value = "";
+        Form.time.value = "";
+        Form.category.value = "";
+    },
+    
+    submit(event) {
+        event.preventDefault();
+
+        try {
+            const {description, time, checked, category} = Form.getValues();
+            List.insert({description, time, checked}, category);
+            Form.clearFields()
+            Modal.toggle();
+
+        } catch(error) {
+            alert(error.message)
+        }
+    }    
+}
+
+
+
